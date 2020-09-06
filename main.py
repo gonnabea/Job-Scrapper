a964d5e8164e179cd2e3441afa4672b38c6ee4eb
Choi Jiwon <winning1993@naver.com>
2020-09-06 09:34:20 +0900

    #2.3 Extracting Indeed Pages

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ from bs4 import BeautifulSoup
 indeed_result = requests.get(
     "https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&radius=25&l=&fromage=any&limit=50")
 
-print(indeed_result.text)
+
+indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
+
+pagination = indeed_soup.find("div", {"class", "pagination"})
+
+pages = pagination.find_all("a")
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+print(spans[:-1])
