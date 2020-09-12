5441738222be1c18555aaaa970cc8eb6b71aa88a
Choi Jiwon <winning1993@naver.com>
2020-09-13 06:19:22 +0900

    #2.11 StackOverflow extract job

--- so.py
+++ so.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = f"https://stackoverflow.com/jobs?q=python&sort"
+URL = f"https://stackoverflow.com/jobs?q=python"
 
 
 def get_last_page():
@@ -14,14 +14,29 @@ def get_last_page():
     return int(last_page)
 
 
+def extract_job(html):
+    title = html.find("h2", {"class": "fs-body3"}).find("a")["title"]
+    company_name = html.find("h3", {"class": "fs-body1"}
+                             ).find("span").get_text(strip=True)
+    company_location = html.find("span", {"class": "fc-black-500"})
+    print(title)
+
+
 def extract_jobs(last_page):
     jobs = []
+
     for page in range(last_page):
-        result = requests.get(f"{URL}={page+1}")
+        result = requests.get(f"{URL}$pg={page+1}")
+
         soup = BeautifulSoup(result.text, "html.parser")
+
         results = soup.find_all("div", {"class": "-job"})
+        print(results)
+
         for result in results:
-            print(result["data-jobid"])
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
 
 def get_jobs():
