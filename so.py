4f265e69e802a1e042c5363c4c6e8f77974b244c
Choi Jiwon <winning1993@naver.com>
2020-09-13 15:01:10 +0900

    Bug Fix

--- so.py
+++ so.py
@@ -15,23 +15,29 @@ def get_last_page():
 
 
 def extract_job(html):
-    title = html.find("h2", {"class": "fs-body3"}).find("a")["title"]
-    company_name = html.find("h3", {"class": "fs-body1"}
-                             ).find("span").get_text(strip=True)
-    company_location = html.find("span", {"class": "fc-black-500"})
+    title = html.find("h2").find("a")["title"]
+    company, location = html.find("h3").find_all("span", recursive=False)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True)
+    job_id = html['data-jobid']
     print(title)
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        'apply_link': f"https://stackoverflow.com/jobs/{job_id}"
+    }
 
 
 def extract_jobs(last_page):
     jobs = []
 
     for page in range(last_page):
-        result = requests.get(f"{URL}$pg={page+1}")
+        result = requests.get(f"{URL}$pg=page+1")
 
         soup = BeautifulSoup(result.text, "html.parser")
 
         results = soup.find_all("div", {"class": "-job"})
-        print(results)
 
         for result in results:
             job = extract_job(result)
